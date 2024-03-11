528cedfce9711d7d3a79105008d2380b0884d68c
AdrianD <powstaniecx02@gmail.com>
2024-03-11 23:52:17 +0100

    Third update

--- ChatSolution/ChatClientApp/MVVM/View/LoginView.xaml.cs
+++ ChatSolution/ChatClientApp/MVVM/View/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ChatClientApp.MVVM.View;
 
@@ -7,5 +8,30 @@ public partial class LoginView : UserControl
     public LoginView()
     {
         InitializeComponent();
+    }
+    private void LoginButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Pobierz wprowadzone dane z TextBoxów
+        string email = EmailTextBox.Text;
+        string password = PasswordBox.Password;
+
+        // Przykładowa logika uwierzytelniania
+        if (AuthenticateUser(email, password))
+        {
+            MessageBox.Show("Zalogowano pomyślnie!");
+            // Tutaj możesz otworzyć nowe okno lub wykonać inne operacje po zalogowaniu
+        }
+        else
+        {
+            MessageBox.Show("Błąd logowania. Sprawdź dane i spróbuj ponownie.");
+        }
+    }
+
+    private bool AuthenticateUser(string email, string password)
+    {
+        // Tutaj zaimplementuj logikę uwierzytelniania
+        // Możesz sprawdzić dane w bazie danych, pliku konfiguracyjnym itp.
+        // W tym przykładzie zawsze zwracamy true dla celów demonstracyjnych
+        return true;
     }
 }
